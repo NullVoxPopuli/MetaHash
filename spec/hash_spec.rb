0ba2d790e975390f158686d35b27b8666d43ef1f
NullVoxPopuli <LPSego3+dev@gmail.com>
2014-09-12 16:14:37 -0400

    added another spec to verify to_metadata

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -4,6 +4,13 @@ describe Hash do
   context "to_metadata" do
     subject(:h){ { a: 1 }.to_metadata }
 
-    it { is_expected.to be_a Metadata }
+    it "is a Metadata" do
+      expect(h).to be_a Metadata
+    end
+
+    it "behaves like a Metdata" do
+      expect(h.a).to eq 1
+    end
   end
+
 end
